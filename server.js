5bc5120830be279ed45715a6f0939ee9cc7a107c
David Rivera A <davidriveraarbelaez@gmail.com>
2024-08-06 20:40:14 -0500

    serverUpdate

--- server.js
+++ server.js
@@ -18,12 +18,24 @@ app.use(express.json());
 /* Parse requests of content-type - application/x-www-form-urlencoded */
 app.use(express.urlencoded({extended:true})); 
 
+const db = require("./app/models");
+db.mongoose
+    .connect(db.url,{
+        useNewUrlParser: true,
+        useUnifiedTopology : true
+    })
+    .then(() => {
+        console.log("DB connected succesfully");
+    })
+    .catch(err => {
+        console.log("DB connection not succesful", err);
+        process.exit();
+    });
+
 // Simple route
 app.get("/", (req,res)=>{
     res.json({ message: "Hello there"})
 });
-
-
 
 // Setting the port, listen for requests
 const PORT = process.eventNames.PORT || 9080;
